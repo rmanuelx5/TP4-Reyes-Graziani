6d5162d987086b24676a0c0f05765b05e9ec9122
Rmanuelx5 <reyesmanuelx5@gmail.com>
2024-07-01 15:12:16 -0300

    Carga archivos iniciales

---
+++ Microchip/Microchip/main.c
@@ -0,0 +1,18 @@
+/*
+ * Microchip.c
+ *
+ * Created: 1/7/2024 15:10:14
+ * Author : Barcala
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
